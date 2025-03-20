77d95bf9a1c578ef20968d4960aad2c699db2ca3
KimDongYeoun <icecream210011@gmail.com>
2025-03-20 21:58:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-#pip install python-dotenv
-#pip install langchain-openai
 import streamlit as st
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
-#환경 변수 로드
+# 환경 변수 로드
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
-#Openai 모델 설정
+# OpenAI 모델 설정
 chat_model = ChatOpenAI(openai_api_key=api_key)
 
-#streamlit UI 구성
+# Streamlit UI 구성
 st.title("AI시 생성기")
 st.write("주제를 입력하면 AI가 맛있는 시를 호로록!")
 
-#사용자 입력 받기
+# 사용자 입력 받기
 jujae = st.text_input("주제를 입력해주세요.")
+person = st.text_input("원하는 스타일의 시인을 입력하세요")
 
-prompt = f"{jujae}를 주제로 시를 써줄래, 윤동주 느낌으로"
+prompt = f"{jujae}를 주제로 시를 써줄래, {person} 느낌으로 해줘."
 
-#출력 드가자!!!!!
-if jujae:
-    try:
-        response = chat_model.invoke(prompt)
-        st.subheader("생선된 시")
-        st.write(response.content)
-    except Exception as e:
-        st.error(f"오류 발생: {e}")
-
+# "시 생성" 버튼을 추가
+if st.button("시 생성"):
+    if not jujae:
+        st.error("주제를 입력해주세요!")
+    else:
+        try:
+            response = chat_model.invoke(prompt)
+            st.subheader("생성된 시")
+            st.write(response.content)
+        except Exception as e:
+            st.error(f"오류 발생: {e}")
