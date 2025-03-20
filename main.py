4f9b94a548362b0e9d75f7f38cac6b9e1eb4c534
KimDongYeoun <icecream210011@gmail.com>
2025-03-20 21:16:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 #Openai 모델 설정
-chat_model = ChatOpenAI(api_key=api_key)
+chat_model = ChatOpenAI(openai_api_key=api_key)
 
 #streamlit UI 구성
 st.title("AI시 생성기")
-st.write("주제를 입력하면 AI가 시를 생성해줍니다.")
+st.write("주제를 입력하면 AI가 맛있는 시를 호로록!")
 
 #사용자 입력 받기
 jujae = st.text_input("주제를 입력해주세요.")
 
 prompt = f"{jujae}를 주제로 시를 써줄래, 윤동주 느낌으로"
 
-#출력 
+#출력 드가자!!!!!
 if jujae:
     try:
         response = chat_model.invoke(prompt)
